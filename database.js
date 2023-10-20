8bf2913bf9e2ee1b363f600a32139e68548cebae
OrcinusX <orcinus.comp@gmail.com>
2023-10-19 22:35:12 -0300

    dataBase Creation

---
+++ database.js
@@ -0,0 +1,189 @@
+{
+    const partes_computadoras = [
+        {
+            ID: 0,
+            NOMBRE: "Procesador Intel Core i7",
+            PRECIO: 299.99,
+            DESCRIPCION_CORTA: "Procesador de alto rendimiento",
+            IMG: "imagen1.jpg"
+        },
+        {
+            ID: 1,
+            NOMBRE: "Memoria RAM Kingston 8GB DDR4",
+            PRECIO: 49.99,
+            DESCRIPCION_CORTA: "Memoria de 8GB para PC",
+            IMG: "imagen2.jpg"
+        },
+        {
+            ID: 2,
+            NOMBRE: "Disco Duro Seagate 1TB",
+            PRECIO: 59.99,
+            DESCRIPCION_CORTA: "Disco duro de 1TB",
+            IMG: "imagen3.jpg"
+        },
+        {
+            ID: 3,
+            NOMBRE: "Tarjeta Gráfica NVIDIA GeForce RTX 3080",
+            PRECIO: 699.99,
+            DESCRIPCION_CORTA: "Tarjeta gráfica de alta gama",
+            IMG: "imagen4.jpg"
+        },
+        {
+            ID: 4,
+            NOMBRE: "Monitor Dell 27\" 4K",
+            PRECIO: 349.99,
+            DESCRIPCION_CORTA: "Monitor 4K de 27 pulgadas",
+            IMG: "imagen5.jpg"
+        },
+        {
+            ID: 5,
+            NOMBRE: "Teclado mecánico Logitech G Pro X",
+            PRECIO: 129.99,
+            DESCRIPCION_CORTA: "Teclado mecánico para juegos",
+            IMG: "imagen6.jpg"
+        },
+        {
+            ID: 6,
+            NOMBRE: "Ratón inalámbrico Logitech MX Master 3",
+            PRECIO: 99.99,
+            DESCRIPCION_CORTA: "Ratón inalámbrico de alta precisión",
+            IMG: "imagen7.jpg"
+        },
+        {
+            ID: 7,
+            NOMBRE: "Procesador Intel Core i7",
+            PRECIO: 299.99,
+            DESCRIPCION_CORTA: "Procesador de alto rendimiento",
+            IMG: "imagen1.jpg",
+            CATEGORIA: "Procesadores"
+        },
+        {
+            ID: 8,
+            NOMBRE: "Memoria RAM Kingston 8GB DDR4",
+            PRECIO: 49.99,
+            DESCRIPCION_CORTA: "Memoria de 8GB para PC",
+            IMG: "imagen2.jpg",
+            CATEGORIA: "Memoria RAM"
+        },
+        {
+            ID: 9,
+            NOMBRE: "Disco Duro Seagate 1TB",
+            PRECIO: 59.99,
+            DESCRIPCION_CORTA: "Disco duro de 1TB",
+            IMG: "imagen3.jpg",
+            CATEGORIA: "Almacenamiento"
+        },
+        {
+            ID: 10,
+            NOMBRE: "Tarjeta Gráfica NVIDIA GeForce RTX 3080",
+            PRECIO: 699.99,
+            DESCRIPCION_CORTA: "Tarjeta gráfica de alta gama",
+            IMG: "imagen4.jpg",
+            CATEGORIA: "Tarjetas Gráficas"
+        },
+        {
+            ID: 11,
+            NOMBRE: "Monitor Dell 27\" 4K",
+            PRECIO: 349.99,
+            DESCRIPCION_CORTA: "Monitor 4K de 27 pulgadas",
+            IMG: "imagen5.jpg",
+            CATEGORIA: "Monitores"
+        },
+        {
+            ID: 12,
+            NOMBRE: "Teclado mecánico Logitech G Pro X",
+            PRECIO: 129.99,
+            DESCRIPCION_CORTA: "Teclado mecánico para juegos",
+            IMG: "imagen6.jpg",
+            CATEGORIA: "Periféricos"
+        },
+        {
+            ID: 13,
+            NOMBRE: "Ratón inalámbrico Logitech MX Master 3",
+            PRECIO: 99.99,
+            DESCRIPCION_CORTA: "Ratón inalámbrico de alta precisión",
+            IMG: "imagen7.jpg",
+            CATEGORIA: "Periféricos"
+        },
+        {
+            ID: 14,
+            NOMBRE: "Placa Base ASUS Prime B450M-A",
+            PRECIO: 79.99,
+            DESCRIPCION_CORTA: "Placa base Micro-ATX para Ryzen",
+            IMG: "imagen8.jpg",
+            CATEGORIA: "Placas Base"
+        },
+        {
+            ID: 15,
+            NOMBRE: "Fuente de Alimentación EVGA 650W",
+            PRECIO: 69.99,
+            DESCRIPCION_CORTA: "Fuente de alimentación de 650W",
+            IMG: "imagen9.jpg",
+            CATEGORIA: "Fuentes de Alimentación"
+        },
+        {
+            ID: 16,
+            NOMBRE: "SSD Samsung 500GB",
+            PRECIO: 79.99,
+            DESCRIPCION_CORTA: "Unidad de estado sólido de 500GB",
+            IMG: "imagen10.jpg",
+            CATEGORIA: "Almacenamiento"
+        },
+        {
+            ID: 17,
+            NOMBRE: "Gabinete NZXT H510",
+            PRECIO: 99.99,
+            DESCRIPCION_CORTA: "Gabinete para PC ATX",
+            IMG: "imagen11.jpg",
+            CATEGORIA: "Gabinetes"
+        },
+        {
+            ID: 18,
+            NOMBRE: "Webcam Logitech C920",
+            PRECIO: 79.99,
+            DESCRIPCION_CORTA: "Cámara web de alta definición",
+            IMG: "imagen12.jpg",
+            CATEGORIA: "Periféricos"
+        },
+        {
+            ID: 19,
+            NOMBRE: "Auriculares HyperX Cloud II",
+            PRECIO: 89.99,
+            DESCRIPCION_CORTA: "Auriculares para juegos con sonido envolvente",
+            IMG: "imagen13.jpg",
+            CATEGORIA: "Auriculares"
+        },
+        {
+            ID: 20,
+            NOMBRE: "Impresora HP LaserJet Pro",
+            PRECIO: 199.99,
+            DESCRIPCION_CORTA: "Impresora láser monocromática",
+            IMG: "imagen14.jpg",
+            CATEGORIA: "Impresoras"
+        },
+        {
+            ID: 21,
+            NOMBRE: "Tarjeta de Red TP-Link WiFi 6",
+            PRECIO: 49.99,
+            DESCRIPCION_CORTA: "Tarjeta de red inalámbrica de alta velocidad",
+            IMG: "imagen15.jpg",
+            CATEGORIA: "Tarjetas de Red"
+        },
+        {
+            ID: 22,
+            NOMBRE: "Sistema de Enfriamiento Cooler Master Hyper 212",
+            PRECIO: 39.99,
+            DESCRIPCION_CORTA: "Sistema de enfriamiento para CPU",
+            IMG: "imagen16.jpg",
+            CATEGORIA: "Refrigeración"
+        },
+        {
+            ID: 23,
+            NOMBRE: "Teclado Inalámbrico Logitech K780",
+            PRECIO: 59.99,
+            DESCRIPCION_CORTA: "Teclado inalámbrico multitipo",
+            IMG: "imagen17.jpg",
+            CATEGORIA: "Periféricos"
+        },
+    ]
+}
